d6dc6862c764736cb5e018716953c5158114e911
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-07-24 16:58:40 +0100

    Update ficha2_consultasMedicas.sql

--- ficha2_consultasMedicas.sql
+++ ficha2_consultasMedicas.sql
@@ -42,3 +42,59 @@ select * from utente where morada like "%tras%" 'or morada like "%trás%";
 ''bloquear os acentos na pesquisa, fazendo que só elementos com o acento surjam'
 
 select * from utente where morada like "%trás" COLLATE utf8_bin;
+
+`3.10 - todas as consultas não realizadas`
+
+SELECT * FROM consulta_medica WHERE NOT realizada;
+
+`3.11 - mostre todas as consultas que se realizaram fora do periodo de 2015/06/12 a 2016/07/29`
+
+SELECT * FROM consulta_medica WHERE data_hora NOT BETWEEN "2015-06-12" AND "2016-07-19";
+
+`3.12 - mostre todos os utentes portugueses e espanhóis`
+SELECT * FROM utente WHERE nacionalidade in ("portuguesa", "espanhola");
+
+`3.13 - mostra todos os utentes que não tem telefone ou não tem telemóvel`
+SELECT * FROM utente WHERE telefone is NULL or telemovel IS NULL;
+
+`3.14 - lista os utentes que tem telefone e telemovel`
+SELECT * FROM utente WHERE telefone is NOT NULL AND telemovel IS NOT NULL;
+
+`4.1 - listar consultas por ordem ascendente da data`
+SELECT * FROM consulta_medica order by data_hora ASC:
+
+`4.2 - listar medicos, ordenado pelo nome de forma ascendente e em caso de colisão, pela data de nascimento asc`
+SELECT * FROM medico order by nome asc and order by data_nascimento asc;
+SELECT * FROM medico order by nome asc, data_nascimento asc;
+
+`5.1- Todas as especialidades são influenciadas em 5% no preço` 
+SELECT especialidade_codigo, especialidade_designacao, (especialidade_preco * 1.05) as especialidade_preco_inflacionado from especialidade order by especialidade_preco_inflacionado asc;
+
+`5.2 - liste o iva de cada uma das especialidades, considerando que cada especialidade teria uma taxa de 23%`
+select especialidade_codigo, especialidade_designacao, especialidade_preco, (especialidade_preco - especialidade_preco / 1.23) as IVA from especialidade;
+
+`6.1 - liste as datas e apenas as datas de todas as consultas de 2015`
+select date(data_year) from consulta_medica where year(data_hora) = 2015;
+
+`6.2 - liste todas as horas (apenas as horas) das consultas realizadas no segundo semestre `
+select hour(data_hora) from consulta_medica where year(data_hora) = 2015 and month(data_year) between 6 and 12;
+
+`6.3 - liste os utentes (codigo) que tenham sido consultados apos as 17h`
+select codigo, data_hora from consulta_medica where hour(data_hora) > 17;
+
+`6.4 - liste o nome e num de telefone de cada utente, se não tiver telefone , resultado = sem telefone`
+select nome, ifnull(telefone, "sem telefone") as telefone from utente;
+
+`6.5 - Liste o nome de todos os utentes com o respectivo nif , ex : "José moreira - nif xxxxxxxx"`
+select concat(nome, " - ", nif) as agregado_nome_nif from utente;
+
+`6.6 - liste o ultimo dia do mes em que cada utente faz anos`
+select nome, day(last_day(data_nascimento)) as "Ultimo dia do mes do ano de nascimento" from utente;
+
+`6.7 - Mostre todas as consultas com excepção dos dias 2 e 10 sept e 2 de março.`
+select * from consulta_medica where date(data_hora) not in ("2015-09-10", "2015-09-02", "2015-03-02");
+
+`6.8 - mostre todas as consultas que ocorrem nos dias 13 e 22 julho de qualquer ano e ainda dia 21 outubro`
+select * from consulta_medica where day(data_hora) in (13,21,22) and month(data_hora) = 07 or date(data_hora) = "2015-10-21";
+
+`7.1 - calcule a idade de todos os utentes`
