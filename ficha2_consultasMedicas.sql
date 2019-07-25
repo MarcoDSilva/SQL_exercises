7d2cc123ddd127cf94e468c2218779a2ef9fe745
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-07-25 16:57:41 +0100

    Update ficha2_consultasMedicas.sql
    bug fixes, updating new exercises

--- ficha2_consultasMedicas.sql
+++ ficha2_consultasMedicas.sql
@@ -1,48 +1,3 @@
-`2.1 - todos os dados dos medicos`
-select * from medico;
-
-`2.2 - todos os dados das consultas medicas`
-select * from consulta_medica;
-
-`2.3 - nome e contactos dos utentes`
-select nome, telefone, telemovel from utente;
-
-`2.4 - numero de consultas e data `
-select data_hora, codigo, utente from consulta_medica;
-
-`2.5 - selecione o utente e medico, e veja porque aparece(ou nao)`
-select utente, medico from consulta_medica;
-
-`selecionar o nome e utente das consultas, em vez dos ids`
-select utente.nome , medico.nome from consulta_medica 
-INNER JOIN utente ON utente.nome = consulta_medica.utente
-INNER JOIN medico ON medico.nome = consulta_medica.medico;
-
-`3.1 - medicos nascidos apos 10 dez 1969`
-select * from medico where data_nascimento > "1969-12-10";
-
-`3.2 - medico com atividade iniciada entre 2005 e 2009`
-select * from medico where data_inicio_atividade between "2005-01-01" and "2009-12-31";
-
-`3.3 - utentes com nacionalidade portuguesa`
-select * from utente where nacionalidade = "Portuguesa";
-
-`3.4 - utentes cujo primeiro nome é Maria`
-select * from utente where nome like "Maria %";
-
-`3.5 - utentes cujo nome de familia é Santoro`
-select * from utente where nome like "% Santoro";
-
-`3.6 - utentes com nome Galvão em qualquer parte do nome`
-select * from utente where nome like "% Galvão %";
-
-`3.7 - utentes que moren na rua trás, com ou sem acento`
-select * from utente where morada like "%tras%" 'or morada like "%trás%";
-
-''bloquear os acentos na pesquisa, fazendo que só elementos com o acento surjam'
-
-select * from utente where morada like "%trás" COLLATE utf8_bin;
-
 `3.10 - todas as consultas não realizadas`
 
 SELECT * FROM consulta_medica WHERE NOT realizada;
@@ -80,7 +35,7 @@ select date(data_year) from consulta_medica where year(data_hora) = 2015;
 select hour(data_hora) from consulta_medica where year(data_hora) = 2015 and month(data_year) between 6 and 12;
 
 `6.3 - liste os utentes (codigo) que tenham sido consultados apos as 17h`
-select codigo, data_hora from consulta_medica where hour(data_hora) > 17;
+select codigo, data_hora from consulta_medica where hour(data_hora) >= 17;
 
 `6.4 - liste o nome e num de telefone de cada utente, se não tiver telefone , resultado = sem telefone`
 select nome, ifnull(telefone, "sem telefone") as telefone from utente;
@@ -98,3 +53,26 @@ select * from consulta_medica where date(data_hora) not in ("2015-09-10", "2015-
 select * from consulta_medica where day(data_hora) in (13,21,22) and month(data_hora) = 07 or date(data_hora) = "2015-10-21";
 
 `7.1 - calcule a idade de todos os utentes`
+select nome, floor(datediff(now(), date(data_nascimento)) / 365) as idade from utente;
+
+`7.2 - formatar , numero utente, nome e idade`
+select numero_utente, nome, concat(floor(datediff(now(), date(data_nascimento)) / 365), ' anos') as idade from utente;
+
+`7.3 - listar especialidades em que se o preço da especialidade é inferior a 68€, é influenciada a 7%`
+select especialidade_designacao, if(especialidade_preco < 68, especialidade_preco * 1.07, especialidade_preco) as preco from especialidade;
+
+`8.1 - liste o nome e código de todos os utentes consultados`
+select utente.nome as nome, consulta_medica.utente as codigo_utente, 
+consulta_medica.codigo as codigo_consulta from consulta_medica
+INNER JOIN utente on utente.numero_utente = consulta_medica.utente;
+`8.2 - mostre o código e o nome de todos os utentes consultados sem repetidos`
+
+`TOFIX`
+
+`9.1 - mostre as 3 primeiras consultas feitas na clinica`
+select * from consulta_medica limit 0, 3;
+
+`9.2 - faça uma lista da terceira e quarta consulta médica do ano 2015`
+select * from consulta_medica where year(data_hora) = 2015 limit 2,2;
+
+`10.1 - mostre o nome da especialidade de cada médico`
